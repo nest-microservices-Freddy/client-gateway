cadcddaec0e67905c80d478a4427f37a2f3f404c
fredsalv01 <freddy.morales@usil.pe>
2024-05-28 07:57:47 -0500

    orders client gateway

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,32 +1,64 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Inject } from '@nestjs/common';
-import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Inject,
+  ParseUUIDPipe,
+  Query,
+} from '@nestjs/common';
 import { ORDER_SERVICE } from '../config';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { CreateOrderDto, OrderPaginationDto, StatusDto } from './dto';
+import { firstValueFrom } from 'rxjs';
+import { PaginationDto } from '../common';
 
 @Controller('orders')
 export class OrdersController {
   constructor(
     @Inject(ORDER_SERVICE)
-    private readonly orderClient: ClientProxy
+    private readonly orderClient: ClientProxy,
   ) {}
 
   @Post()
   create(@Body() createOrderDto: CreateOrderDto) {
     return this.orderClient.send('createOrder', createOrderDto);
-    // return this.ordersService.create(createOrderDto);
   }
 
   @Get()
-  findAll() {
-    return this.orderClient.send('findAllOrders', {});
-    // return this.ordersService.findAll();
+  findAll(@Query() orderPaginationDto: OrderPaginationDto) {
+    // return orderPaginationDto;
+    return this.orderClient.send('findAllOrders', orderPaginationDto);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.orderClient.send('findOneOrder', {});
-    // return this.ordersService.findOne(+id);
+  @Get('id/:id')
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+    try {
+      const order = await firstValueFrom(
+        this.orderClient.send('findOneOrder', { id }),
+      );
+      return order;
+    } catch (error) {
+      throw new RpcException(error);
+    }
   }
 
+  @Get(':status')
+  async findAllByStatus(
+    @Param() statusDto: StatusDto,
+    @Query() paginationDto: PaginationDto,
+  ) {
+    try {
+      const order = await firstValueFrom(
+        this.orderClient.send('findAllOrders', {
+          ...paginationDto,
+          status: statusDto.status
+        })
+      );
+      return order;
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
 }
